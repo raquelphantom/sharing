d5b231b668e3b46b3795bf2fbb6d39e29a5d0138
WagneyJ655 <j.wagney@aluno.ifsp.edu.br>
2025-06-19 23:44:00 -0300

    Update PEDIR.c
    FOI KRL

--- PEDIR.c
+++ PEDIR.c
@@ -3,36 +3,43 @@
 #include <stdlib.h> 
 #include <string.h>
 
-typedef struct {
-	int cod;
-	char descr[20+1];
-	float custo;
-	float total;
-    int qtd;
-}CARDAPIO;
+#define TAMSTR 201
 
- /* PROTÓTIPOS DE FUNÇÕES */
+typedef struct {
+	int		codprod;
+	char	descrprod[TAMSTR];
+	float	custoprod;
+}REGISTRO;
+
+typedef struct {
+	REGISTRO r;
+	int qtd;
+	float total;
+}
+PRODUTO;
+ /* PROTÃ“TIPOS DE FUNÃ‡Ã•ES */
  int busca (int c);
  void menu (void);
  void pick (void);
- void GravaPedido (CARDAPIO reg);
+ void GravaPedido (REGISTRO r);
  
 /*VARIAVEIS GLOBAIS*/
-CARDAPIO reg;
+	REGISTRO r;
+	PRODUTO p;
 
 int busca (int c){
 	int flag;
 	FILE *arq;
 	flag = 0;
-	arq = fopen("PRODUTOS.DAT","r");//r - lê
+	arq = fopen("PRODUTOS.DAT","r");//r - lÃª
 	if(arq == NULL)
 	{
 		printf("\n erro ao abrir, my friend");
 		getch();
 		exit(0);
 	}//(EOF) fim do arquivo - true ou false
-while (fread(&reg, sizeof(reg), 1, arq) == 1) {
-    if (reg.cod == c) {
+while (fread(&r, sizeof(r), 1, arq) == 1) {
+    if (r.codprod == c) {
         flag = 1;
         break;
     }
@@ -43,47 +50,48 @@ while (fread(&reg, sizeof(reg), 1, arq) == 1) {
 
 void menu (void){
 	FILE *arq;
-	arq = fopen("PRODUTOS.DAT","r");//r - lê
+	arq = fopen("PRODUTOS.DAT","r");//r - lÃª
 	if(arq == NULL)
 	{
 		printf("\n erro ao abrir, my friend");
 		getch();
 		exit(0);
 	}//(EOF) fim do arquivo - true ou false
-	while (fread(&reg, sizeof(reg), 1, arq) == 1) 
+	while (fread(&r, sizeof(r), 1, arq) == 1) 
 	{
-    	printf("\n %i \t %s \t R$%.2f", reg.cod, reg.descr, reg.custo);
+    	printf("\n %i \t %s \t R$%.2f", r.codprod,r.descrprod , r.custoprod);
 	}
 	fclose(arq);
 }
 
 void pick (void){
-     menu();// mostra cardápio lendo o prod.dat
-	printf("\nFaça seu pedido:  ");
-	fflush(stdin); scanf("%i", &reg.cod);
-	if(busca(reg.cod) == 1){
-		printf("\n Quantas unidades de %s voce deseja?", reg.descr);
-			fflush(stdin); scanf("%i", &reg.qtd);
-			printf("\nTotal parcial: R$ %.2f", reg.qtd*reg.custo);
-			reg.total = reg.total+reg.qtd*reg.custo;
-			printf("\nTotal do pedido: %.2f",reg.total);
+
+     menu();// mostra cardÃ¡pio lendo o prod.dat
+	printf("\nFaÃ§a seu pedido:  ");
+	fflush(stdin); scanf("%i", &r.codprod);
+	if(busca(r.codprod) == 1){
+		printf("\n Quantas unidades de %s voce deseja?", r.descrprod);
+			fflush(stdin); scanf("%i", &p.qtd);
+			printf("\nTotal parcial: R$ %.2f", p.qtd*r.custoprod);
+			p.total = p.total+p.qtd*r.custoprod;
+			printf("\nTotal do pedido: %.2f",p.total);
 	}
 	else{
-		printf("produto não encontrado");
+		printf("produto nÃ£o encontrado");
 	}
 }
 
-void GravaPedido (CARDAPIO reg)
+void GravaPedido (REGISTRO r)
 {
 	FILE * Arq;
-	Arq = fopen ("PEDIDO.DAT", "w");
+	Arq = fopen ("PEDIDO.DAT", "a");
 		if (Arq == NULL ) 
 	{
 		printf ("\nErro ao ler arquivo");
 		getch();
         exit(0);
 	}
-	fwrite(&reg, sizeof(reg), 1, Arq);//escreve
+	fwrite(&p, sizeof(p), 1, Arq);//escreve
 	fclose(Arq);//fecha
 	
 }   
@@ -91,10 +99,10 @@ void GravaPedido (CARDAPIO reg)
 int main(){
 	char opc;
 	do{
-		pick();//função pra escolher
+	pick();//funÃ§Ã£o pra escolher
 		printf("\ndeseja adicionar outro produto?  [s/n]");
 		fflush(stdin); opc = getche();
 	}while (opc != 'n' && opc != 'N' );
-	GravaPedido (reg);
+	GravaPedido (r);
     return 0;
 }
